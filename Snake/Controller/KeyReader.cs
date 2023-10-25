27ab5026a015822f4a462a8ea82e86776e6343b9
YaraMm <for.steam2016@mail.ru>
2023-10-25 22:40:35 +0300

    Controller is done

---
+++ Snake/Controller/KeyReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Snake.Controller
+{
+    public class KeyReader
+    {
+        public Action GetAction()
+        {
+            if(Console.KeyAvailable == true)
+            {
+                var key = Console.ReadKey(true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.A:
+                        return Action.Left;
+
+                    case ConsoleKey.S:
+                        return Action.Down;
+
+                    case ConsoleKey.D:
+                        return Action.Right;
+
+                    case ConsoleKey.W:
+                        return Action.Up;
+                    default:
+                        return Action.None;
+                }
+            }
+            return Action.None;
+        }
+    }
+}
+
