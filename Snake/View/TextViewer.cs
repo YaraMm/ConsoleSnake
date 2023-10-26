fa5ff856058c61a4768e9b31e0feb1d3fb8c5649
YaraMm <for.steam2016@mail.ru>
2023-10-27 00:17:06 +0300

    draw border

--- Snake/View/TextViewer.cs
+++ Snake/View/TextViewer.cs
@@ -1,5 +1,6 @@
 ﻿using Snake.Model.Entities;
 using System;
+using System.Collections.Generic;
 
 namespace Snake.View
 {
@@ -12,12 +13,29 @@ namespace Snake.View
 
         public void Draw(Frame frame, Border border)
         {
-
+            if(border.Type == BorderType.Horizontal)
+            {
+                var x = border.StartPosition.X;
+                for (int i = x; i < x + border.Length; i++)
+                {
+                    frame.Pixels[border.StartPosition.Y, i] = CharIcons.BorderIcon;
+                }
+            }
+            else
+            {
+                var y = border.StartPosition.Y;
+                for (int i = y; i < y + border.Length; i++)
+                {
+                    frame.Pixels[i, border.StartPosition.X] = CharIcons.BorderIcon;
+                }
+            }
         }
 
         public void Draw(Frame frame, Food food)
         {
-
+            var x = food.Position.X;
+            var y = food.Position.Y;
+            frame.Pixels[y, x] = CharIcons.FoodIcon;
         }
 
         public void Draw(Frame frame, SnakeEntity snake)
